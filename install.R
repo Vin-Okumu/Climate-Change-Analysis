74ec3a79c7e0cc43c5d86baeb9198f0ff8af6068
Vin-Okumu <vinny.otieno23@gmail.com>
2025-03-11 01:05:16 +0300

    Update install.R

--- install.R
+++ install.R
@@ -7,5 +7,5 @@ py_install("kaggle")
 kaggle <- import("kaggle")
 kaggle$api$authenticate()
 
-kaggle$datasets$download("-d vincentokumu/Climate_Change_Indicators", path = "data", unzip = TRUE)
+kaggle$datasets$download("-d vincentokumu/climate-change-modelling-r", path = "data", unzip = TRUE)
 
