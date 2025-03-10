8e096ce6a0193b9df5c24915cc128caf46f6dff6
Vin-Okumu <vinny.otieno23@gmail.com>
2025-03-11 01:02:45 +0300

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,11 @@
 install.packages(c("tidyverse", "ggplot2", "dplyr"))
 
+install.packages("reticulate")
+library(reticulate)
+py_install("kaggle")
+
+kaggle <- import("kaggle")
+kaggle$api$authenticate()
+
+kaggle$datasets$download("-d vincentokumu/Climate_Change_Indicators", path = "data", unzip = TRUE)
+
